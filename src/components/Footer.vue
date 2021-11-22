97ddbd6c67367e22b4a45e26f7026eee7d1cce3d
Sapana Baniya <sapnabaniya.xdezo@gmail.com>
2021-11-22 20:39:24 +0545

    changes

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { isDark, toggleDark } from '~/composables'
+
+const { t, availableLocales, locale } = useI18n()
+
+const toggleLocales = () => {
+  // change to some real logic
+  const locales = availableLocales
+  locale.value = locales[(locales.indexOf(locale.value) + 1) % locales.length]
+}
+</script>
+
+<template>
+</template>
